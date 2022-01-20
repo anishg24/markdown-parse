16d6850ce86eaa9b114852f2e7d49276ab901896
Anish Govind <anishg24@gmail.com>
2022-01-19 17:17:42 -0800

    Fix 1

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,6 +5,19 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+//    public static ArrayList<String> getLinks(String markdown) {
+//        ArrayList<String> toReturn = new ArrayList<>();
+//        int index = 0;
+//
+//        while (index < markdown.length()) {
+//            int startTarget = markdown.indexOf(")[", index);
+//            int endTarget = markdown.indexOf(")", startTarget);
+//            toReturn.add(markdown.substring(startTarget + 1, endTarget));
+//            index = endTarget + 1;
+//        }
+//        return toReturn;
+//    }
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
@@ -15,11 +28,15 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (currentIndex > closeParen) break;
+            else if (currentIndex < closeParen) {
+                currentIndex = closeParen + 1;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
         }
         return toReturn;
     }
+
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
